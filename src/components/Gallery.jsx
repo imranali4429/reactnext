55a64f6552b7b61907fc76e69e4925ce94da5639
imranali4429 <imran4429ali@gmail.com>
2025-05-27 20:13:51 +0600

    add more same photo

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,16 +1,23 @@
 function Profile() {
-    return (
-        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
-    );
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
 }
 
 export default function Gallery() {
-    return (
-        <section>
-            <h1>Amazing scientists</h1>
-            <Profile />
-            <Profile />
-        
-        </section>
-    );
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <br />
+      <Profile />
+      <br />
+      <Profile />
+      <br />
+      <Profile />
+      <br />
+      <Profile />
+      <br />
+      <Profile />
+      <br />
+    </section>
+  );
 }
