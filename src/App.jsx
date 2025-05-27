589e52a4e6c85cb3b66e70467bdfd26d1f9f5b8b
imranali4429 <imran4429ali@gmail.com>
2025-05-27 18:41:45 +0600

    add all for module 1.4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./components/Gallery";
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to my rnext</h1>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App;
